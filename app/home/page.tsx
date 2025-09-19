faad32ceb854266dc7e55447a9509ba548367f48
Alec Faustino <alecfaustino@outlook.com>
2025-09-19 13:58:57 -0600

    add constants for type and cuisine. add useState default to the filers col

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,11 +1,23 @@
+"use client";
 import LeftCol from "@/components/home/LeftCol";
 import MiddleCol from "@/components/home/MiddleCol";
 import RightCol from "@/components/home/RightCol";
-
+import { useState } from "react";
 export default function Home() {
+  const [filters, setFilters] = useState({
+    type: "",
+    cuisine: "",
+    diet: [],
+    intolerances: [],
+    maxCalories: 2000,
+    maxProtein: 500,
+    maxCarbs: 500,
+    maxFat: 500,
+  });
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4 items-center">
-      <LeftCol />
+      <LeftCol setFilters={setFilters} />
       <MiddleCol />
       <RightCol />
     </div>
