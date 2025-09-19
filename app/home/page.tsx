f5dcd19dca6c96ec8bdef9b2306d4e58b93ae104
Alec Faustino <alecfaustino@outlook.com>
2025-09-19 07:24:55 -0600

    fetch from the spoonacular API successfully

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,7 +4,7 @@ import RightCol from "@/components/home/RightCol";
 
 export default function Home() {
   return (
-    <div>
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4 items-center">
       <LeftCol />
       <MiddleCol />
       <RightCol />
--- components/home/MiddleCol.tsx
+++ components/home/MiddleCol.tsx
@@ -1,7 +1,50 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+import { Card, CardContent } from "../ui/card";
+
 export default function MiddleCol() {
+  const loadingRef = useRef(false);
+  const [recipes, setRecipes] = useState<[]>([]);
+  const baseUrl = "https://api.apilayer.com/spoonacular";
+
+  const fetchRecipe = async () => {
+    loadingRef.current = true;
+    try {
+      const fetchResult = await fetch(
+        `${baseUrl}/recipes/complexSearch?number=1`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            apikey: process.env.NEXT_PUBLIC_SPOONACULAR_API_KEY || "",
+          },
+        }
+      );
+      const data = await fetchResult.json();
+      const recipe = data.results;
+      setRecipes(recipe);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      console.log("reached finally block");
+      loadingRef.current = false;
+    }
+  };
+
+  useEffect(() => {
+    fetchRecipe();
+  }, []);
+
   return (
-    <>
-      <h1> Hello from middle Col</h1>
-    </>
+    <Card>
+      <CardContent>
+        {recipes?.map((recipe: any) => (
+          <div key={recipe.id}>
+            <h2>{recipe.title}</h2>
+          </div>
+        ))}
+        <h2>Something is here</h2>
+      </CardContent>
+    </Card>
   );
 }
