85e22f71f2909c53be9108ca46e4f2994cc2f8bc
Alec Faustino <alecfaustino@outlook.com>
2025-09-19 07:29:25 -0600

    render image and set next config file

--- components/home/MiddleCol.tsx
+++ components/home/MiddleCol.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useRef, useState } from "react";
 import { Card, CardContent } from "../ui/card";
+import Image from "next/image";
 
 export default function MiddleCol() {
   const loadingRef = useRef(false);
@@ -39,8 +40,14 @@ export default function MiddleCol() {
     <Card>
       <CardContent>
         {recipes?.map((recipe: any) => (
-          <div key={recipe.id}>
-            <h2>{recipe.title}</h2>
+          <div key={recipe.id} className="text-center space-y-4">
+            <h2 className="text-lg font-bold">{recipe.title}</h2>
+            <Image
+              src={recipe.image}
+              alt={recipe.title}
+              width={500}
+              height={300}
+            />
           </div>
         ))}
         <h2>Something is here</h2>
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["spoonacular.com", "img.spoonacular.com"], // add any domains you fetch images from
+  },
 };
 
 export default nextConfig;
