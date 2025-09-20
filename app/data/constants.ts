8f00857889f1ef1c44ea8f14ce001e1fe88ee66c
Alec Faustino <alecfaustino@outlook.com>
2025-09-19 19:31:47 -0600

    add select filters

--- app/data/constants.ts
+++ app/data/constants.ts
@@ -44,3 +44,35 @@ export const cuisines: string[] = [
   "Thai",
   "Vietnamese"
 ];
+
+
+
+
+export const diets: string[] = [
+  "Gluten Free",
+  "Ketogenic",
+  "Vegetarian",
+  "Lacto-Vegetarian",
+  "Ovo-Vegetarian",
+  "Vegan",
+  "Pescetarian",
+  "Paleo",
+  "Primal",
+  "Low FODMAP",
+  "Whole30",
+];
+
+export const intolerances: string[] = [
+  "Dairy",
+  "Egg",
+  "Gluten",
+  "Grain",
+  "Peanut",
+  "Seafood",
+  "Sesame",
+  "Shellfish",
+  "Soy",
+  "Sulfite",
+  "Tree Nut",
+  "Wheat",
+];
--- components/home/LeftCol.tsx
+++ components/home/LeftCol.tsx
@@ -3,6 +3,22 @@
 import { SetStateAction } from "react";
 import { Card, CardContent, CardHeader, CardFooter } from "../ui/card";
 import { Button } from "../ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+
+import {
+  diets,
+  cuisines,
+  intolerances,
+  mealTypes,
+} from "../../app/data/constants";
 
 interface LeftColProps {
   setFilters: React.Dispatch<SetStateAction<any>>;
@@ -10,12 +26,55 @@ interface LeftColProps {
 
 export default function LeftCol({ setFilters }: LeftColProps) {
   return (
-    <Card>
-      <CardHeader className="text-lg font-bold">Filters</CardHeader>
-      <CardContent></CardContent>
-      <CardFooter>
-        <Button variant="outline">Apply Filters</Button>
-      </CardFooter>
-    </Card>
+    <div className="text-center items-center">
+      <Card>
+        <CardHeader className="text-lg font-bold">Filters</CardHeader>
+        <CardContent>
+          {/* Meal Type */}
+          <label className="block mb-2 font-medium">Meal Type</label>
+          <div className="flex justify-center mb-4">
+            <Select>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Meal Type" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Meal Types</SelectLabel>
+                  {mealTypes.map((type) => (
+                    <SelectItem key={type} value={type}>
+                      {type}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+
+          {/* Cuisine */}
+          <label className="block mb-2 font-medium">Cuisine</label>
+
+          <div className="flex justify-center mb-4">
+            <Select>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Cuisine" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Cuisines</SelectLabel>
+                  {cuisines.map((cuisine) => (
+                    <SelectItem key={cuisine} value={cuisine}>
+                      {cuisine}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+        </CardContent>
+        <CardFooter className="flex justify-center">
+          <Button variant="outline">Apply Filters</Button>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
