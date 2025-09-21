bbe586c6e1b41fd0b8304e6b3a4130c290fe03b6
Alec Faustino <alecfaustino@outlook.com>
2025-09-21 14:33:38 -0600

    implement the shadcn darkmode and put the button on the navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,9 +28,15 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {/* Temporary header */}
-        <Navbar />
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange>
+          {/* Temporary header */}
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,19 @@
 import Link from "next/link";
+import DarkButton from "./darkmode/DarkButton";
 export default function Navbar() {
   return (
-    <nav className="w-full sticky top-0 left-0 z-50 h-16 bg-white/80 backdrop-blur-md shadow-md">
+    <nav className="w-full sticky top-0 left-0 z-50 h-16 bg-white/80 dark:bg-gray-900/80 backdrop-blur-sm border-b border-gray-200/20 dark:border-gray-700/20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center justify-between">
         {/* Logo / Brand */}
-        <div className="text-xl font-bold text-gray-800">
+        <div className="text-xl font-bold">
           <Link href="/">NOMinate</Link>
         </div>
 
         {/* Nav Links */}
         <div className="hidden md:flex space-x-6">
-          <Link href="/" className="text-gray-700 hover:text-gray-900">
-            Home
-          </Link>
-          <Link href="/about" className="text-gray-700 hover:text-gray-900">
-            Favorites
-          </Link>
+          <Link href="/">Home</Link>
+          <Link href="/about">Favorites</Link>
+          <DarkButton />
         </div>
       </div>
     </nav>
