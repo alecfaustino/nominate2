9eded90e73e6c47a38e5c920e1d2426e4254670b
Alec Faustino <alecfaustino@outlook.com>
2025-09-27 15:47:31 -0600

    add height to loading skeleton to be visible

--- components/home/Loading.tsx
+++ components/home/Loading.tsx
@@ -1,5 +1,5 @@
 import { Skeleton } from "../ui/skeleton";
 
 export default function Loading() {
-  return <Skeleton className="h-full w-full animate-pulse rounded-md" />;
+  return <Skeleton className="h-8 w-full animate-pulse rounded-md" />;
 }
